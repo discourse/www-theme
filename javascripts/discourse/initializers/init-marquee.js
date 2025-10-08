5b2303db7273f31f1ee241550993181d662425c4
Jarek Radosz <jarek@cvx.dev>
2025-10-08 23:37:10 +0200

    DEV: Update linting

--- javascripts/discourse/initializers/init-marquee.js
+++ javascripts/discourse/initializers/init-marquee.js
@@ -1,6 +1,6 @@
 import { apiInitializer } from "discourse/lib/api";
 import Marquee from "../components/marquee";
 
-export default apiInitializer("1.8.0", (api) => {
+export default apiInitializer((api) => {
   api.renderInOutlet("above-site-header", Marquee);
 });
